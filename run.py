84de6b22795ab16c9221261e5df5a991b94cf1cb
221830083 <221830083@smail.nju.edu.cn>
2025-05-09 16:26:18 +0800

    修改文件结构

--- run.py
+++ run.py
@@ -3,9 +3,9 @@ import ir_datasets
 from pyserini.search.lucene import LuceneSearcher
 from pyserini.search._base import get_topics
 from llmrankers.rankers import SearchResult
-from llmrankers.pointwise import PointwiseLlmRanker, MonoT5LlmRanker
-from llmrankers.setwise import SetwiseLlmRanker, OpenAiSetwiseLlmRanker
-from llmrankers.pairwise import PairwiseLlmRanker, DuoT5LlmRanker, OpenAiPairwiseLlmRanker
+# from llmrankers.pointwise import PointwiseLlmRanker, MonoT5LlmRanker
+# from llmrankers.setwise import SetwiseLlmRanker, OpenAiSetwiseLlmRanker
+# from llmrankers.pairwise import PairwiseLlmRanker, DuoT5LlmRanker, OpenAiPairwiseLlmRanker
 from llmrankers.listwise import OpenAiListwiseLlmRanker, ListwiseLlmRanker
 from tqdm import tqdm
 import argparse
@@ -51,69 +51,69 @@ def write_run_file(path, results, tag):
 
 def main(args):
 
-    if args.pointwise:
-        if 'monot5' in args.run.model_name_or_path:
-            ranker = MonoT5LlmRanker(model_name_or_path=args.run.model_name_or_path,
-                                     tokenizer_name_or_path=args.run.tokenizer_name_or_path,
-                                     device=args.run.device,
-                                     cache_dir=args.run.cache_dir,
-                                     method=args.pointwise.method,
-                                     batch_size=args.pointwise.batch_size)
-        else:
-            ranker = PointwiseLlmRanker(model_name_or_path=args.run.model_name_or_path,
-                                        tokenizer_name_or_path=args.run.tokenizer_name_or_path,
-                                        device=args.run.device,
-                                        cache_dir=args.run.cache_dir,
-                                        method=args.pointwise.method,
-                                        batch_size=args.pointwise.batch_size)
+    # if args.pointwise:
+    #     if 'monot5' in args.run.model_name_or_path:
+    #         ranker = MonoT5LlmRanker(model_name_or_path=args.run.model_name_or_path,
+    #                                  tokenizer_name_or_path=args.run.tokenizer_name_or_path,
+    #                                  device=args.run.device,
+    #                                  cache_dir=args.run.cache_dir,
+    #                                  method=args.pointwise.method,
+    #                                  batch_size=args.pointwise.batch_size)
+    #     else:
+    #         ranker = PointwiseLlmRanker(model_name_or_path=args.run.model_name_or_path,
+    #                                     tokenizer_name_or_path=args.run.tokenizer_name_or_path,
+    #                                     device=args.run.device,
+    #                                     cache_dir=args.run.cache_dir,
+    #                                     method=args.pointwise.method,
+    #                                     batch_size=args.pointwise.batch_size)
 
-    elif args.setwise:
-        if args.run.openai_key:
-            ranker = OpenAiSetwiseLlmRanker(model_name_or_path=args.run.model_name_or_path,
-                                            api_key=args.run.openai_key,
-                                            num_child=args.setwise.num_child,
-                                            method=args.setwise.method,
-                                            k=args.setwise.k)
-        else:
-            ranker = SetwiseLlmRanker(model_name_or_path=args.run.model_name_or_path,
-                                      tokenizer_name_or_path=args.run.tokenizer_name_or_path,
-                                      device=args.run.device,
-                                      cache_dir=args.run.cache_dir,
-                                      num_child=args.setwise.num_child,
-                                      scoring=args.run.scoring,
-                                      method=args.setwise.method,
-                                      num_permutation=args.setwise.num_permutation,
-                                      k=args.setwise.k)
+    # elif args.setwise:
+    #     if args.run.openai_key:
+    #         ranker = OpenAiSetwiseLlmRanker(model_name_or_path=args.run.model_name_or_path,
+    #                                         api_key=args.run.openai_key,
+    #                                         num_child=args.setwise.num_child,
+    #                                         method=args.setwise.method,
+    #                                         k=args.setwise.k)
+    #     else:
+    #         ranker = SetwiseLlmRanker(model_name_or_path=args.run.model_name_or_path,
+    #                                   tokenizer_name_or_path=args.run.tokenizer_name_or_path,
+    #                                   device=args.run.device,
+    #                                   cache_dir=args.run.cache_dir,
+    #                                   num_child=args.setwise.num_child,
+    #                                   scoring=args.run.scoring,
+    #                                   method=args.setwise.method,
+    #                                   num_permutation=args.setwise.num_permutation,
+    #                                   k=args.setwise.k)
 
-    elif args.pairwise:
-        if args.pairwise.method != 'allpair':
-            args.pairwise.batch_size = 2
-            logger.info(f'Setting batch_size to 2.')
+    # if args.pairwise:
+    #     if args.pairwise.method != 'allpair':
+    #         args.pairwise.batch_size = 2
+    #         logger.info(f'Setting batch_size to 2.')
 
-        if args.run.openai_key:
-            ranker = OpenAiPairwiseLlmRanker(model_name_or_path=args.run.model_name_or_path,
-                                             api_key=args.run.openai_key,
-                                             method=args.pairwise.method,
-                                             k=args.pairwise.k)
+    #     if args.run.openai_key:
+    #         ranker = OpenAiPairwiseLlmRanker(model_name_or_path=args.run.model_name_or_path,
+    #                                          api_key=args.run.openai_key,
+    #                                          method=args.pairwise.method,
+    #                                          k=args.pairwise.k)
 
-        elif 'duot5' in args.run.model_name_or_path:
-            ranker = DuoT5LlmRanker(model_name_or_path=args.run.model_name_or_path,
-                                    tokenizer_name_or_path=args.run.tokenizer_name_or_path,
-                                    device=args.run.device,
-                                    cache_dir=args.run.cache_dir,
-                                    method=args.pairwise.method,
-                                    batch_size=args.pairwise.batch_size,
-                                    k=args.pairwise.k)
-        else:
-            ranker = PairwiseLlmRanker(model_name_or_path=args.run.model_name_or_path,
-                                       tokenizer_name_or_path=args.run.tokenizer_name_or_path,
-                                       device=args.run.device,
-                                       cache_dir=args.run.cache_dir,
-                                       method=args.pairwise.method,
-                                       batch_size=args.pairwise.batch_size,
-                                       k=args.pairwise.k)
+    #     elif 'duot5' in args.run.model_name_or_path:
+    #         ranker = DuoT5LlmRanker(model_name_or_path=args.run.model_name_or_path,
+    #                                 tokenizer_name_or_path=args.run.tokenizer_name_or_path,
+    #                                 device=args.run.device,
+    #                                 cache_dir=args.run.cache_dir,
+    #                                 method=args.pairwise.method,
+    #                                 batch_size=args.pairwise.batch_size,
+    #                                 k=args.pairwise.k)
+    #     else:
+    #         ranker = PairwiseLlmRanker(model_name_or_path=args.run.model_name_or_path,
+    #                                    tokenizer_name_or_path=args.run.tokenizer_name_or_path,
+    #                                    device=args.run.device,
+    #                                    cache_dir=args.run.cache_dir,
+    #                                    method=args.pairwise.method,
+    #                                    batch_size=args.pairwise.batch_size,
+    #                                    k=args.pairwise.k)
 
-    elif args.listwise:
+    if args.listwise:
         if args.run.openai_key:
             ranker = OpenAiListwiseLlmRanker(model_name_or_path=args.run.model_name_or_path,
                                              api_key=args.run.openai_key,
@@ -130,7 +130,8 @@ def main(args):
                                        scoring=args.run.scoring,
                                        num_repeat=args.listwise.num_repeat)
     else:
-        raise ValueError('Must specify either --pointwise, --setwise, --pairwise or --listwise.')
+        raise ValueError(
+            'Must specify either --pointwise, --setwise, --pairwise or --listwise.')
 
     query_map = {}
     if args.run.ir_dataset_name is not None:
@@ -145,8 +146,10 @@ def main(args):
         topics = get_topics(args.run.pyserini_index+'-test')
         for topic_id in list(topics.keys()):
             text = topics[topic_id]['title']
-            query_map[str(topic_id)] = ranker.truncate(text, args.run.query_length)
-        docstore = LuceneSearcher.from_prebuilt_index(args.run.pyserini_index+'.flat')
+            query_map[str(topic_id)] = ranker.truncate(
+                text, args.run.query_length)
+        docstore = LuceneSearcher.from_prebuilt_index(
+            args.run.pyserini_index+'.flat')
 
     logger.info(f'Loading first stage run from {args.run.run_path}.')
     first_stage_rankings = []
@@ -157,7 +160,8 @@ def main(args):
             qid, _, docid, _, score, _ = line.strip().split()
             if qid != current_qid:
                 if current_qid is not None:
-                    first_stage_rankings.append((current_qid, query_map[current_qid], current_ranking[:args.run.hits]))
+                    first_stage_rankings.append(
+                        (current_qid, query_map[current_qid], current_ranking[:args.run.hits]))
                 current_ranking = []
                 current_qid = qid
             if len(current_ranking) >= args.run.hits:
@@ -172,8 +176,10 @@ def main(args):
                 if 'title' in data:
                     text = f'{data["title"]} {text}'
             text = ranker.truncate(text, args.run.passage_length)
-            current_ranking.append(SearchResult(docid=docid, score=float(score), text=text))
-        first_stage_rankings.append((current_qid, query_map[current_qid], current_ranking[:args.run.hits]))
+            current_ranking.append(SearchResult(
+                docid=docid, score=float(score), text=text))
+        first_stage_rankings.append(
+            (current_qid, query_map[current_qid], current_ranking[:args.run.hits]))
 
     reranked_results = []
     total_comparisons = 0
@@ -188,7 +194,8 @@ def main(args):
             elif args.run.shuffle_ranking == 'inverse':
                 ranking = ranking[::-1]
             else:
-                raise ValueError(f'Invalid shuffle ranking method: {args.run.shuffle_ranking}.')
+                raise ValueError(
+                    f'Invalid shuffle ranking method: {args.run.shuffle_ranking}.')
         reranked_results.append((qid, query, ranker.rerank(query, ranking)))
         total_comparisons += ranker.total_compare
         total_prompt_tokens += ranker.total_prompt_tokens
@@ -197,7 +204,8 @@ def main(args):
 
     print(f'Avg comparisons: {total_comparisons/len(reranked_results)}')
     print(f'Avg prompt tokens: {total_prompt_tokens/len(reranked_results)}')
-    print(f'Avg completion tokens: {total_completion_tokens/len(reranked_results)}')
+    print(
+        f'Avg completion tokens: {total_completion_tokens/len(reranked_results)}')
     print(f'Avg time per query: {(toc-tic)/len(reranked_results)}')
 
     write_run_file(args.run.save_path, reranked_results, 'LLMRankers')
@@ -208,8 +216,10 @@ if __name__ == '__main__':
     commands = parser.add_subparsers(title='sub-commands')
 
     run_parser = commands.add_parser('run')
-    run_parser.add_argument('--run_path', type=str, help='Path to the first stage run file (TREC format) to rerank.')
-    run_parser.add_argument('--save_path', type=str, help='Path to save the reranked run file (TREC format).')
+    run_parser.add_argument(
+        '--run_path', type=str, help='Path to the first stage run file (TREC format) to rerank.')
+    run_parser.add_argument(
+        '--save_path', type=str, help='Path to save the reranked run file (TREC format).')
     run_parser.add_argument('--model_name_or_path', type=str,
                             help='Path to the pretrained model or model identifier from huggingface.co/models')
     run_parser.add_argument('--tokenizer_name_or_path', type=str, default=None,
@@ -222,8 +232,10 @@ if __name__ == '__main__':
     run_parser.add_argument('--device', type=str, default='cuda')
     run_parser.add_argument('--cache_dir', type=str, default=None)
     run_parser.add_argument('--openai_key', type=str, default=None)
-    run_parser.add_argument('--scoring', type=str, default='generation', choices=['generation', 'likelihood'])
-    run_parser.add_argument('--shuffle_ranking', type=str, default=None, choices=['inverse', 'random'])
+    run_parser.add_argument(
+        '--scoring', type=str, default='generation', choices=['generation', 'likelihood'])
+    run_parser.add_argument('--shuffle_ranking', type=str,
+                            default=None, choices=['inverse', 'random'])
 
     pointwise_parser = commands.add_parser('pointwise')
     pointwise_parser.add_argument('--method', type=str, default='yes_no',
@@ -251,9 +263,11 @@ if __name__ == '__main__':
     args = parse_args(parser, commands)
 
     if args.run.ir_dataset_name is not None and args.run.pyserini_index is not None:
-        raise ValueError('Must specify either --ir_dataset_name or --pyserini_index, not both.')
+        raise ValueError(
+            'Must specify either --ir_dataset_name or --pyserini_index, not both.')
 
     arg_dict = vars(args)
     if arg_dict['run'] is None or sum(arg_dict[arg] is not None for arg in arg_dict) != 2:
-        raise ValueError('Need to set --run and can only set one of --pointwise, --pairwise, --setwise, --listwise')
+        raise ValueError(
+            'Need to set --run and can only set one of --pointwise, --pairwise, --setwise, --listwise')
     main(args)
